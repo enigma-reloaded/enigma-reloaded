c0a444f62c5d56a2f8e0994a273e7e9ad2152d24
Enigma Reloaded <enigma-reloaded@users.noreply.github.com>
2021-08-09 22:28:20 +0100

    add react app

---
+++ app/src/app.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <div className="text-red-500">
+      ok
+    </div>
+  )
+}
---
+++ app/src/app.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import App from './app';
+
+test('renders', () => {
+  render(<App />);
+});
