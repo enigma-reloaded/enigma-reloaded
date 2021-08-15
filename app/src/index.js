ecff79523eb62e7580c5afec12bb6a5be74f60f0
Enigma Reloaded <enigma-reloaded@users.noreply.github.com>
2021-08-15 09:08:33 +0100

    update

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './tailwind.css'
 import App from './app';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
+
 reportWebVitals();
