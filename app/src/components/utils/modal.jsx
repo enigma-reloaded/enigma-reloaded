58401be466193bbad4f2e53c3596d16400086643
Enigma Reloaded <enigma-reloaded@users.noreply.github.com>
2021-08-30 15:22:44 +0100

    use portal for modal

--- app/src/components/utils/modal.jsx
+++ app/src/components/utils/modal.jsx
@@ -1,7 +1,9 @@
-import {useEffect, useRef} from 'react';
+import {createPortal} from 'react-dom';
+import {useEffect, useMemo, useRef} from 'react';
 import useOnClickOutside from 'use-onclickoutside';
 
 export default function Modal({isOpened, close, children}) {
+  const el = useMemo(() => document.getElementById('modal-portal'), []);
   useEffect(() => {
     if (isOpened) {
       return document.body.classList.add('overflow-y-hidden');
@@ -14,15 +16,17 @@ export default function Modal({isOpened, close, children}) {
 
   if (!isOpened) return null;
 
-  return (
+  const output = (
     <>
       <div className="absolute inset-0 bg-gray-500 opacity-70"/>
 
-      <div className="absolute inset-0 items-center py-20 overflow-y-auto px-2 md:px-0">
-        <div className="mb-20 bg-white md:w-1/3 w-full p-4 rounded shadow mx-auto" ref={modalContentRef}>
+      <div className="absolute inset-0 flex items-center py-20 overflow-y-auto px-2 md:px-0">
+        <div className="md:mb-10 bg-white md:w-1/3 w-full p-4 rounded shadow mx-auto" ref={modalContentRef}>
           {children}
         </div>
       </div>
     </>
   );
+
+  return createPortal(output, el);
 }
